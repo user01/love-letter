c3bd9bef7bed67a93b557bc34c7b9b31a0523e4a
Erik <erik.langenborg@gmail.com>
2017-03-23 18:50:01 -0400

    Laid out basic tests

---
+++ loveletter/tests/test_game.py
@@ -0,0 +1,14 @@
+"""Tests for the main Love Letter game"""
+
+import unittest
+
+
+class TestInit(unittest.TestCase):
+    """Initialization of the Love Letter Game"""
+
+    def test_addition(self):
+        """Example Test"""
+        self.assertEqual(1 + 0, 1)
+
+if __name__ == '__main__':
+    unittest.main()
