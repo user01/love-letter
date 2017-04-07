9b1e4dde1ef2afcb09f9ee640cdd38af93b036e9
Erik <erik.langenborg@gmail.com>
2017-04-07 17:32:20 -0400

    Basic game new()

--- loveletter/tests/test_game.py
+++ loveletter/tests/test_game.py
@@ -2,6 +2,8 @@
 
 import unittest
 from loveletter.game import Game
+from loveletter.card import Card
+from loveletter.player import PlayerAction
 
 
 class TestInit(unittest.TestCase):
@@ -21,6 +23,31 @@ class TestStatics(unittest.TestCase):
         self.assertEqual(Game.new_hand_card(2, [1, 2]), 1)
         self.assertEqual(Game.new_hand_card(2, [2, 2]), 2)
 
+
+class TestBasic(unittest.TestCase):
+    """Test of Basic Game operations"""
+
+    def test_new(self):
+        """Getting a new game"""
+        game = Game.new()
+        self.assertEqual(game.draw_card(), Card.guard)
+        self.assertEqual(game.round(), 0)
+        self.assertEqual(game.player_turn(), 0)
+        self.assertEqual(game.cards_left(), 10)
+        self.assertTrue(game.active())
+        self.assertFalse(game.over())
+
+    def test_move_guard_failure(self):
+        """Getting a guard move, with a wrong guess"""
+        game = Game.new()
+        action = PlayerAction(Card.guard, 1, Card.handmaid, 0)
+        game = game.move(action)
+
+        self.assertEqual(game.round(), 0)
+        self.assertEqual(game.player_turn(), 1)
+        self.assertEqual(game.cards_left(), 9)
+        self.assertTrue(game.active())
+        self.assertFalse(game.over())
 
 if __name__ == '__main__':
     unittest.main()
