3b5ee9c6aeb8a3612ec902eb765f1dc3bcf95eaa
Erik <erik.langenborg@gmail.com>
2017-04-27 23:24:39 -0400

    First implementation of DQ RL

---
+++ loveletter/agents/dqn.py
@@ -0,0 +1,439 @@
+# -*- coding: utf-8 -*-
+
+"""
+Deep Q Learning Agent for the a Love Letter AI
+"""
+
+import random
+import math
+import time
+import sys
+from collections import namedtuple
+from operator import itemgetter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from loveletter.game import Game
+from loveletter.card import Card
+from loveletter.player import PlayerAction
+from .agent import Agent
+from .random import AgentRandom
+
+
+class AgentDQN(Agent):
+    '''
+    Agent which leverages Deep Q Learning
+    '''
+
+    def __init__(self,
+                 model_path=None,
+                 seed=451):
+        self._seed = seed
+        self._idx = 0
+        self._model = ModelDQN()
+        ModelDQN.Seed(seed)
+
+        if ModelDQN.USE_CUDA:
+            self._model.cuda()
+
+        if model_path is not None:
+            self._model.load_state_dict(torch.load(model_path))
+
+    def _move(self, game):
+        '''Return move which ends in score hole'''
+        self._idx = self._idx + 1
+        ModelDQN.Seed(self._seed + self._idx)
+
+        state = TrainerDQN.game_to_state(game_clone)
+        action = self._model(
+            ModelDQN.Variable(state.unsqueeze(0))
+        ).data.cpu()
+        move_options = Agent.valid_actions(game_clone, self._idx)
+        move_values = action[0][torch.LongTensor(move_options)].tolist()
+        move_idx = action[0][torch.LongTensor(move_options)].max(0)[
+            1].tolist()[0]
+        move = move_options[move_idx]
+
+        return Game.rotate_board(rot_flag, move)
+
+    @staticmethod
+    def action_by_score(game, scores, seed=451):
+        """
+        Returns best action based on assigned scores
+
+        return (action, score, idx)
+        """
+        if len(scores) != 15:
+            raise Exception("Invalid scores length")
+
+        actions_possible = AgentDQN.actions_set(game, seed)
+
+        actions = [(action, score, idx) for action, score, idx in
+                   zip(actions_possible,
+                       scores,
+                       range(len(actions_possible)))
+                   if game.is_action_valid(action)]
+
+        action = max(actions, key=itemgetter(2))
+        return action
+
+    @staticmethod
+    def actions_possible(game, seed=451):
+        """Returns valid (idx, actions) based on a current game"""
+
+        action_candidates = AgentDQN.actions_set(game, seed)
+
+        actions = [(idx, action) for idx, action in
+                   enumerate(action_candidates)
+                   if game.is_action_valid(action)]
+
+        return actions
+
+    @staticmethod
+    def actions_set(game, seed=451):
+        """Returns all actions for a game"""
+
+        random.seed(seed + game.round())
+        player_self = game.player_turn()
+        opponents = game.opponent_turn()
+
+        actions_possible = [
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.priest,
+                         Card.noCard),
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.baron,
+                         Card.noCard),
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.handmaid,
+                         Card.noCard),
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.prince,
+                         Card.noCard),
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.king,
+                         Card.noCard),
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.countess,
+                         Card.noCard),
+            PlayerAction(Card.guard,
+                         random.choice(opponents),
+                         Card.princess,
+                         Card.noCard),
+            PlayerAction(Card.priest,
+                         random.choice(opponents),
+                         Card.noCard,
+                         Card.noCard),
+            PlayerAction(Card.baron,
+                         random.choice(opponents),
+                         Card.noCard,
+                         Card.noCard),
+            PlayerAction(Card.king,
+                         random.choice(opponents),
+                         Card.noCard,
+                         Card.noCard),
+            PlayerAction(Card.prince,
+                         random.choice(opponents),
+                         Card.noCard,
+                         Card.noCard),
+            PlayerAction(Card.prince, player_self, Card.noCard, Card.noCard),
+            PlayerAction(Card.handmaid, player_self, Card.noCard, Card.noCard),
+            PlayerAction(Card.countess, player_self, Card.noCard, Card.noCard),
+            PlayerAction(Card.princess, player_self, Card.noCard, Card.noCard)
+        ]
+
+        return actions_possible
+
+
+class TrainerDQN():
+    """
+    Training class for simple Deep Q Learning
+
+    Based on
+    http://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+    """
+
+    def __init__(self,
+                 model_path=None,
+                 seed=451,
+                 batch_size=128,
+                 gamma=0.999,
+                 eps_start=0.9,
+                 eps_end=0.05,
+                 eps_decay=200,
+                 replay_size=10000,
+                 learning_rate=0.02):
+        self._run = 0
+        self._steps_done = 0
+        self._seed = seed
+
+        self._BATCH_SIZE = batch_size
+        self._GAMMA = gamma
+        self._EPS_START = eps_start
+        self._EPS_END = eps_end
+        self._EPS_DECAY = eps_decay
+        ModelDQN.Seed(seed)
+
+        self._model = ModelDQN()
+        self._memory = ReplayMemory(replay_size)
+        self._optimizer = optim.RMSprop(
+            self._model.parameters(), lr=learning_rate)
+
+        if ModelDQN.USE_CUDA:
+            self._model.cuda()
+        if model_path is not None:
+            self._model.load_state_dict(torch.load(model_path))
+
+    def write_state_to_path(self, path):
+        torch.save(self._model.state_dict(), path)
+
+    def train(self, num_episodes=10, agent=None, print_mod=1):
+        self._run += 1
+        actions = 0
+        self._steps_done = 0
+        time_start = time.time()
+        time_last = time_start
+        agent = AgentRandom(self._seed + self._run) if agent is None else agent
+
+        for idx in range(num_episodes):
+            # Initialize the environment and state
+            if idx % print_mod == 0 or time.time() - time_last > 30:
+                sys.stdout.write(
+                    " {:0>5}/{:0>5}/{:0>5}:".format(self._steps_done,
+                                                    idx,
+                                                    len(self._memory)))
+                sys.stdout.flush()
+                time_last = time.time()
+            game = Game.new(4, idx)
+
+            while game.active():
+                # Select and perform an action
+                if not game.is_current_player_playing():
+                    game = game.skip_eliminated_player()
+                    continue
+                actions += 1
+                action, action_idx = self._select_action(game)
+                action_tensor = torch.LongTensor([action_idx])
+                game_next, reward = game.move(action)
+
+                # Observe states
+                state_current = torch.from_numpy(game.state())
+                state_next = torch.FloatTensor([-1] + [0] * 23) if \
+                    game_next.over() else torch.from_numpy(game_next.state())
+
+                # Store the transition in memory
+                self._memory.push(state_current,
+                                  action_tensor,
+                                  state_next,
+                                  torch.FloatTensor([reward]))
+
+                game = game_next
+
+                # Perform one step of the optimization (on the target network)
+                self._optimize_model()
+
+        aps = actions / (time.time() - time_start)
+        print(' :EOF {:.2f} action/second'.format(aps))
+
+    def _optimize_model(self):
+
+        if not self._memory.full:
+            return
+        (state_batch, action_batch, state_next_batch, reward_batch) = \
+            self._memory.sample(self._BATCH_SIZE)
+
+        indices_normal = []
+        indices_final = []
+        for idx, value in enumerate(state_next_batch[:, 0]):
+            if value != -1:
+                indices_normal.append(idx)
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        mask_normal = torch.LongTensor(indices_normal)
+        mask_bit_normal = state_next_batch[:, 0] != -1
+        if ModelDQN.USE_CUDA:
+            mask_bit_normal = mask_bit_normal.cuda()
+            # mask_normal = mask_normal.cuda()
+
+        # We don't want to backprop through the expected action values and
+        # volatile will save us on temporarily changing the model parameters'
+        # requires_grad to False!
+        state_batch = ModelDQN.Variable(state_batch)
+        action_batch = ModelDQN.Variable(action_batch)
+        reward_batch = ModelDQN.Variable(reward_batch)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken
+        state_action_values = self._model(state_batch)
+        state_action_values = state_action_values.gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        next_state_values = ModelDQN.Variable(torch.zeros(self._BATCH_SIZE))
+        state_next_batch_normal = ModelDQN.Variable(
+            state_next_batch.index_select(0, mask_normal), True)
+
+        next_state_values[mask_bit_normal] = self._model(
+            state_next_batch_normal).max(1)[0]
+
+        # Now, we don't want to mess up the loss with a volatile flag, so let's
+        # clear it. After this, we'll just end up with a Variable that has
+        # requires_grad=False
+        # next_state_values.volatile = False
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self._GAMMA) + \
+            reward_batch
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values,
+                                expected_state_action_values)
+
+        # if self._memory.position % 200 == 0:
+        #     print("Loss: ", loss.data[0])
+
+        # Optimize the model
+        self._optimizer.zero_grad()
+        loss.backward()
+        for param in self._model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self._optimizer.step()
+
+    @staticmethod
+    def game_to_state(game):
+        """Resolve a game's current state to a model input state"""
+        return torch.FloatTensor(game._board[0:6] + game._board[7:13]).div(48)
+
+    @staticmethod
+    def game_to_reward(score_previous, game, player_two_acted):
+        """Reward based on the past score. Always player 1 POV"""
+        score = game.score()
+        if game.over():
+            reward = 100 if score[0] > score[1] else -100
+        else:
+            # penalize waiting
+            # penalize less if keeping turn
+            # earning score is worth something
+            # letting score is penalized
+            reward = 0 + \
+                (-1 if player_two_acted else 0) + \
+                0.5 * (score[0] - score_previous[0])
+            # reward = 0
+
+        return torch.Tensor([reward])
+
+    @staticmethod
+    def action_tensor_to_int(action):
+        return action[0]
+
+    def _select_action(self, game):
+        """
+        Based on the current model and a game state, pick a new action
+        """
+        sample = random.random()
+        state = torch.from_numpy(game.state()).type(torch.FloatTensor)
+        self._steps_done += 1
+        eps_threshold = self._EPS_END + (self._EPS_START - self._EPS_END) * \
+            math.exp(-1. * self._steps_done / self._EPS_DECAY)
+
+        if sample > eps_threshold:
+            scores = self._model(
+                ModelDQN.Variable(state.unsqueeze(0))
+            ).data.cpu().tolist()[0]
+            action, _, idx = AgentDQN.action_by_score(
+                game, scores, self._steps_done)
+        else:
+            idx, action = random.choice(AgentDQN.actions_possible(game))
+
+        return action, idx
+
+
+class ModelDQN(nn.Module):
+    """The DQN Model. From the 12 slots, pick of the six choices"""
+    USE_CUDA = torch.cuda.is_available()
+
+    def __init__(self):
+        super(ModelDQN, self).__init__()
+        self.layer1 = nn.Linear(24, 32)
+        self.layer2 = nn.Linear(32, 64)
+        self.layer3 = nn.Linear(64, 32)
+        self.layer4 = nn.Linear(32, 15)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+        x = self.layer4(x)
+        return x
+
+    @staticmethod
+    def Variable(tensor, volatile=False):
+        if ModelDQN.USE_CUDA:
+            tensor = tensor.cuda()
+        return Variable(tensor, volatile=volatile)
+
+    @staticmethod
+    def Seed(seed):
+        torch.manual_seed(seed)
+        if ModelDQN.USE_CUDA:
+            torch.cuda.manual_seed_all(seed)
+
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    """
+    Stores the transitions that the agent observes, allowing us to reuse this
+    data later. By sampling from it randomly, the transitions that build up a
+    batch are decorrelated. It has been shown that this greatly stabilizes and
+    improves the DQN training procedure.
+
+    http://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html#replay-memory
+    """
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.position = 0
+        self.states = torch.zeros(capacity, 24)
+        self.actions = torch.zeros(capacity, 1).type(torch.LongTensor)
+        self.states_next = torch.zeros(capacity, 24)
+        self.rewards = torch.zeros(capacity, 1)
+        self.full = False
+        if ModelDQN.USE_CUDA:
+            self.states.cuda()
+            self.actions.cuda()
+            self.states_next.cuda()
+            self.rewards.cuda()
+
+    def push(self, state, action, state_next, reward):
+        """Saves a transition."""
+        self.states[self.position] = state
+        self.actions[self.position] = action
+        self.states_next[self.position] = state_next
+        self.rewards[self.position] = reward
+        self.position = (self.position + 1) % self.capacity
+        self.full = self.full or self.position == 0
+
+    def sample(self, batch_size):
+        """Sample from the memories"""
+        rand_batch = torch.randperm(self.capacity)
+        index = torch.split(rand_batch, batch_size)[0]
+        states_batch = self.states[index]
+        action_batch = self.actions[index]
+        states_next_batch = self.states_next[index]
+        rewards_batch = self.rewards[index]
+        return (states_batch, action_batch, states_next_batch, rewards_batch)
+
+    def __len__(self):
+        return self.capacity if self.full else self.position
---
+++ run_dqn.py
@@ -0,0 +1,92 @@
+
+import argparse
+import json
+import os
+import re
+import datetime
+import subprocess
+
+from tensorboard_logger import configure, log_value
+
+from loveletter.agents.dqn import TrainerDQN, AgentDQN
+from loveletter.arena import Arena
+from loveletter.agents.random import AgentRandom
+
+
+git_head = subprocess.check_output(['git',
+                                    'rev-parse',
+                                    '--short',
+                                    'HEAD']).strip().decode('UTF-8')
+
+configure("runs/{}".format(git_head), flush_secs=5)
+
+
+PARSER = argparse.ArgumentParser(
+    description='Train a DQN agent')
+
+PARSER.add_argument('--verbose', action="store_true", default=True,
+                    help='Log verbose results')
+
+PARSER.add_argument('--epochs', type=int, default=100,
+                    help='Number of epochs to train')
+
+PARSER.add_argument('--input', type=str,
+                    help='Path to read past trained start data')
+
+PARSER.add_argument('--output', type=str, required=True,
+                    help='Path to write trained results')
+
+# ARGS = PARSER.parse_args()
+ARGS = PARSER.parse_args(([
+    '--output',
+    'models/dqn.pth',
+    '--verbose'
+]))
+
+print('Starting Training')
+
+
+def train(epochs, path_output, path_input=None, verbose=True):
+
+    if path_input is not None:
+        epoch_regexp = re.compile(r".*epoch\.(\d+)$")
+        regexp_result = epoch_regexp.match(path_input)
+        starting_epoch = int(regexp_result.groups()[0])
+    else:
+        starting_epoch = 0
+
+    trainer = TrainerDQN(path_input,
+                         seed=451,
+                         batch_size=2048,
+                         gamma=0.9,
+                         eps_start=0.9,
+                         eps_end=0.05,
+                         eps_decay=40000,
+                         replay_size=25000,
+                         learning_rate=0.02)
+
+    for epoch in range(starting_epoch + 1, epochs):
+        print(" Training Epoch {}".format(epoch))
+        trainer.train(4500, print_mod=500)
+        trainer.write_state_to_path(
+            "{}.epoch.{:0>8}".format(path_output, epoch))
+        trainer.write_state_to_path(path_output)
+
+        win_rate_v_random = Arena.compare_agents_float(
+            lambda seed: AgentDQN(path_output, seed + epoch),
+            lambda seed: AgentRandom(seed + epoch),
+            800)
+        msg = " Epoch {: >3} | VsRandom: {: >4}%".format(
+            epoch,
+            round(win_rate_v_random * 100, 2)
+        )
+
+        log_value('win_rate_v_random', win_rate_v_random, epoch)
+
+        print(' ───── ' + datetime.datetime.now().strftime("%c") + ' ───── ')
+        print(msg)
+
+
+train(ARGS.epochs, ARGS.output, ARGS.input, ARGS.verbose)
+
+print('Complete')
